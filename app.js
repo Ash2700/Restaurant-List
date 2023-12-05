6228fc9dcfe52df0e31b42662c00a1138a46e569
VMVU-MSIGAME\許謝建興 <vmvuruvu@gmail.com>
2023-12-05 09:59:47 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get ( '/', (req, res) =>{
-  res.send ("hello ")
+  res.redirect ("/restaurantlist")
+})
+
+app.get( '/restaurantlist' ,(req, res)=> {
+  res.send('what do u want')
+})
+
+app.get('/restaurantlist/:id', (req, res) => {
+  const id =req.params.id
+  res.send(`read movie :${id}`)
 })
 
 app.listen( port ,()=>{
