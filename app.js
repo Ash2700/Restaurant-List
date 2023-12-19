bba61e9c26e4b279d0ab4975d5b77f54ea88aab1
VMVU-MSIGAME\許謝建興 <vmvuruvu@gmail.com>
2023-12-19 10:45:44 +0800

    build: add favorite page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.static('public'))
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.redirect("/restaurantList")
@@ -15,17 +17,24 @@ app.get('/', (req, res) => {
 
 app.get('/restaurantList', (req, res) => {
   const keyword = req.query.keyword?.trim();
-  const matchedRestaurant = keyword ? restaurants.filter((items) =>
+  const matchedRestaurant = keyword ? filterRestaurants(keyword)
+   : restaurants
+  const finalyDate = matchedRestaurant.length > 0 ? matchedRestaurant:restaurants
+  res.render('index', { restaurants: finalyDate, keyword })
+})
+
+function filterRestaurants(keyword){
+  restaurants.filter((items) =>
     Object.values(items).some((property) => {
       if (typeof property === 'string') {
         return property.toLowerCase().includes(keyword.toLowerCase())
       }
       return false
     })
-  ) : restaurants
-  const finalyDate = matchedRestaurant.length > 0 ? matchedRestaurant:restaurants
-  res.render('index', { restaurants: finalyDate, keyword })
-})
+  )
+}
+
+
 
 
 app.get('/restaurants/:id', (req, res) => {
@@ -34,6 +43,35 @@ app.get('/restaurants/:id', (req, res) => {
   )
   res.render('detail', { detail })
 })
+
+app.get('/addFavorite',(req,res) => {
+  res.render('favorite')
+})
+app.post('/submitRestaurantData',(req, res, next)=> {
+  const {name,
+  name_en,
+  category,
+  image,
+  location,
+ phone,
+ google_map,
+  description}=req.body
+  const id =restaurants.length+1
+  const newRestaurant ={
+    id ,
+    name,
+    name_en,
+    category,
+    image,
+    location,
+    phone,
+    google_map,
+    description
+  };
+  restaurants.push(newRestaurant)
+  res.render('favorite')
+})
+
 
 app.listen(port, () => {
   console.log(`express server is running HTTP://localhost:${port}`)
