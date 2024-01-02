7c87bce6836ad8608ad51a18df6933bd8800d753
VMVU-MSIGAME\許謝建興 <vmvuruvu@gmail.com>
2024-01-02 15:01:05 +0800

    clean code and try handlebars

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
 const { Sequelize } = require('sequelize')
 const db = require('./models')
 const restaurant = db.restaurant
+const pageParameter = require('./public/jsons/pageParameter.json').parameter
 
-app.use(express.static('public'))
+
+
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
+app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
@@ -18,10 +22,9 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   res.redirect("/restaurants")
 })
-
+//home page
 app.get('/restaurants', (req, res) => {
   const keyword = req.query.keyword?.trim();
-
   const matchedRestaurant = keyword ? filterFormDatabaseByKeyword(keyword)
     : findAllFormDatabase()
   return matchedRestaurant.then((restaurant_sqlData) => {
@@ -33,41 +36,21 @@ app.get('/restaurants', (req, res) => {
 })
 
 async function filterFormDatabaseByKeyword(keyword) {
-  const Op=Sequelize.Op
+  const Op = Sequelize.Op
   return await restaurant.findAll({
-      attributes: ['id', 'name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'rating', 'description'],
-      raw: true,
-      where: {
-        [Op.or]:[
-          { name: {[Op.substring]: `${keyword}`}  },
-          { name_en: {[Op.substring]: `${keyword}`} },
-          { category: {[Op.substring]: `${keyword}`} },
-        ]
-      
+    attributes: ['id', 'name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'rating', 'description'],
+    raw: true,
+    where: {
+      [Op.or]: [
+        { name: { [Op.substring]: `${keyword}` } },
+        { name_en: { [Op.substring]: `${keyword}` } },
+        { category: { [Op.substring]: `${keyword}` } },
+      ]
+
     }
   })
 }
-//撈出分類資料
-const getCategoriesFormDatabase = async () => {
-  try {
-    const data = Object.values(await restaurant.findAll({ attributes: ['category'], raw: true }))
-    return data.map((item) => item.category)
-  } catch (error) {
-    console.error('發生錯誤', error)
-    throw error
-  }
-}
-//篩選出單一資料
-const getUniqueCategories = async () => {
-  try {
-    const categoriesArray = await getCategoriesFormDatabase();
-    const uniqueArray = [...new Set(categoriesArray)];
-    return uniqueArray;
-  } catch (error) {
-    console.error('發生錯誤', error);
-    throw error;
-  }
-}
+
 //撈全部屬性資料出來
 async function findAllFormDatabase() {
   return await restaurant.findAll({
@@ -82,34 +65,39 @@ async function findIdFormDatabase(id) {
     raw: true
   })
 }
-//home page
-app.get('/restaurants/add', async (req, res) => {
-  try {
-    const categories = await getUniqueCategories()
-    console.log(categories)
-    res.render('favorite', { categories: categories })
-  } catch (error) {
-    console.error('發生錯誤', error)
-  }
+//新增收藏資料
 
+app.get('/restaurants/add',  (req, res) => {
+  const formAction = "/restaurants/add"
+    res.render('favorite', { formAction })
+ 
+})
+//創建新資料
+app.post('/restaurants/add', (req, res) => {
+  const body = req.body
+  return restaurant.create(body)
+    .then(() => res.redirect('/restaurants/add'))
+    .catch((err) => { console.log(err) })
 })
 //顯示編輯按鈕
 app.get('/restaurants/edit', (req, res) => {
   const keyword = req.query.keyword?.trim();
+  const display= true
   return findAllFormDatabase()
     .then((restaurant_sqlData) => {
-      res.render('showEditButton', {
-        restaurants: restaurant_sqlData, keyword
+      res.render('index', {
+        restaurants: restaurant_sqlData, keyword,display
       })
     })
     .catch((err) => { console.log(err) })
 })
-
 //編輯資料頁
 app.get('/restaurants/edit/:id', (req, res) => {
   const id = req.params.id
+  const formAction = "/restaurants/edit/{{detail.id}}?_method=PUT"
   return findIdFormDatabase(id)
-    .then((detail) => { res.render('detailEdit', { detail }) })
+    .then((detail) => { 
+      res.render('favorite', { info:detail,formAction,parameter:pageParameter }) })
     .catch((err) => { console.log(err) })
 })
 //更新資料
@@ -126,6 +114,7 @@ app.delete('/restaurants/edit/:id', (req, res) => {
   return restaurant.destroy({ where: { id } })
     .then(() => { res.redirect('/restaurants/edit') })
 })
+
 //顯次詳細資料
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
@@ -133,13 +122,7 @@ app.get('/restaurants/:id', (req, res) => {
     .then((detail) => { res.render('detail', { detail }) })
     .catch((err) => { console.log(err) })
 })
-//創建新資料
-app.post('/restaurants', (req, res) => {
-  const body = req.body
-  return restaurant.create(body)
-    .then(() => res.redirect('/restaurants/add'))
-    .catch((err) => { console.log(err) })
-})
+
 
 
 app.listen(port, () => {
---
+++ public/javascripts/help.js
@@ -0,0 +1,9 @@
+
+
+Handlebars.registerHelper("if", function (info,options) {
+  console.log(info.length)
+  console.log(info)
+
+
+  
+});
