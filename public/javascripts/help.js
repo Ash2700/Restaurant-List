ab1377b404664a59b3d133a1f87d53516780a1f5
Ash2700 <vmvuruvu@gmail.com>
2024-01-04 21:03:15 +0800

    輸入驗證

--- public/javascripts/help.js
+++ public/javascripts/help.js
@@ -1,4 +1,16 @@
-Handlebars.registerHelper('if', function (info, options) {
-  console.log(info.length)
-  console.log(info)
-})
+
+const submitButton = document.querySelector("#submitButton");
+const form = document.querySelector("#dataForm");
+
+form.addEventListener("submit", function onFormSubmitted(event) {
+  document.querySelector("#dataForm").classList.add("was-validated");
+
+  form.querySelectorAll("input").forEach((element) => {
+    if (!element.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+      const feedback = element.parentElement.querySelector(".invalid-feedback");
+      feedback.textContent = element.validationMessage;
+    }
+  });
+});
